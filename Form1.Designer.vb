d4944cfe26a2a5dc6ea67762b71ea002ff519be5
Admin <Admin@R6>
2025-03-21 19:06:26 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        CheckBox1 = New CheckBox()
+        Button1 = New Button()
+        GroupBox1 = New GroupBox()
+        GroupBox2 = New GroupBox()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        Button2 = New Button()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label1.Location = New Point(172, 35)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(37, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Login"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label2.Location = New Point(0, 19)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(105, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "UserName/E-mail"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label3.Location = New Point(39, 71)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(59, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Password"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(111, 16)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(111, 63)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(111, 125)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(108, 19)
+        CheckBox1.TabIndex = 5
+        CheckBox1.Text = "Show Password"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(111, 163)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 6
+        Button1.Text = "Log In"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = Color.FromArgb(CByte(255), CByte(255), CByte(192))
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(CheckBox1)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Location = New Point(35, 63)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(242, 242)
+        GroupBox1.TabIndex = 7
+        GroupBox1.TabStop = False
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.BackColor = Color.FromArgb(CByte(255), CByte(255), CByte(192))
+        GroupBox2.BackgroundImageLayout = ImageLayout.None
+        GroupBox2.Controls.Add(Button2)
+        GroupBox2.Controls.Add(TextBox6)
+        GroupBox2.Controls.Add(TextBox5)
+        GroupBox2.Controls.Add(TextBox4)
+        GroupBox2.Controls.Add(TextBox3)
+        GroupBox2.Controls.Add(Label8)
+        GroupBox2.Controls.Add(Label7)
+        GroupBox2.Controls.Add(Label6)
+        GroupBox2.Controls.Add(Label5)
+        GroupBox2.Location = New Point(309, 63)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(289, 242)
+        GroupBox2.TabIndex = 8
+        GroupBox2.TabStop = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label4.Location = New Point(445, 34)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(52, 15)
+        Label4.TabIndex = 9
+        Label4.Text = "Sign-Up"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label5.Location = New Point(31, 19)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(50, 15)
+        Label5.TabIndex = 0
+        Label5.Text = "E-mail : "
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label6.Location = New Point(31, 112)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(65, 15)
+        Label6.TabIndex = 1
+        Label6.Text = "Password :"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label7.Location = New Point(31, 71)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(72, 15)
+        Label7.TabIndex = 2
+        Label7.Text = "UserName :"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label8.Location = New Point(0, 152)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(107, 15)
+        Label8.TabIndex = 3
+        Label8.Text = "Confirm Password"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(109, 19)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(123, 23)
+        TextBox3.TabIndex = 4
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(109, 63)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(123, 23)
+        TextBox4.TabIndex = 5
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(109, 104)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(123, 23)
+        TextBox5.TabIndex = 6
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(109, 144)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(123, 23)
+        TextBox6.TabIndex = 7
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(136, 187)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 8
+        Button2.Text = "Sign Up"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(255), CByte(224), CByte(192))
+        ClientSize = New Size(610, 357)
+        Controls.Add(Label4)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Login Page"
+        TransparencyKey = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox6 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Dim flag As String
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If flag = "Log In" Then
+            Dim sqlconn As New SqlConnection("Data Source=R6\SQLEXPRESS;Initial Catalog=ADONET;Integrated Security=True;Encrypt=False")
+            sqlconn.Open()
+            Dim sql As String
+            sql = "SELECT FirstName, Password from Forms WHERE FirstName= '" & Me.TextBox1.Text & "', Password= '" & Me.TextBox2.Text & "')"
+
+
+        End If
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+End Class
